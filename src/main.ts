cdf8c543b64c181a102ceb3b60ded41c17719a6f
Martín Cruz <martincruz1426@gmail.com>
2024-07-20 17:22:03 -0300

    Updating main file config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { RpcCustomExceptionFilter } from './shared';
 
 async function bootstrap() {
+  const logger = new Logger('Main-Gateway');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalFilters(new RpcCustomExceptionFilter());
+
+  await app.listen(envs.PORT);
+
+  logger.log(`Gateway running on port ${envs.PORT}`);
 }
 bootstrap();
