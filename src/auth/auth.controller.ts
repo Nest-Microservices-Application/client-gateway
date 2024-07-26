b1254e5b1297cfef9817b58677a01da22bad90a4
Martín Cruz <martincruz1426@gmail.com>
2024-07-26 01:59:04 -0300

    Connecting client gateway with auth microservice implementation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post, UseGuards } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { NATS_SERVICE } from '../config';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  registerUser() {
-    return this.client.send('auth.register.user', {}).pipe(
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto).pipe(
       catchError((err) => {
         throw new RpcException(err);
       }),
@@ -17,20 +21,17 @@ export class AuthController {
   }
 
   @Post('login')
-  loginUser() {
-    return this.client.send('auth.login.user', {}).pipe(
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto).pipe(
       catchError((err) => {
         throw new RpcException(err);
       }),
     );
   }
 
+  @UseGuards(AuthGuard)
   @Get('verify')
-  verifyToken() {
-    return this.client.send('auth.verify.user', {}).pipe(
-      catchError((err) => {
-        throw new RpcException(err);
-      }),
-    );
+  verifyToken(@User() user: CurrentUser, @Token() token: string) {
+    return { user, token };
   }
 }
